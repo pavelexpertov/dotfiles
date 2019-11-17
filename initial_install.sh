28f564852797e6c2547df4ed2371f17fa65d357e
pavelexpertov <pavelm01@hotmail.co.uk>
2019-11-17 22:38:08 +0000

    Add some basic configuration files

---
+++ initial_install.sh
@@ -0,0 +1,4 @@
+sudo apt install -y steam
+sudo apt install -y entr git zsh python python3
+# For the pyenv dependencies
+sudo apt install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
