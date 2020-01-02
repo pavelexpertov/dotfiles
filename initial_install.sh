6cde9b8c54082e34dfe9609a0df8b8075f9278de
pavelexpertov <tobe@filled.in>
2020-01-02 15:10:11 +0000

    update initial_install.sh script

--- initial_install.sh
+++ initial_install.sh
@@ -1,4 +1,4 @@
 sudo apt install -y steam
-sudo apt install -y entr git zsh python python3
+sudo apt install -y entr zsh python python3 autojump silversearcher-ag tmux
 # For the pyenv dependencies
 sudo apt install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
