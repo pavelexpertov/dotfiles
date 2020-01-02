9eaa3dbd60f67737a7c2a2afcca9da857eb76001
pavelexpertov <pavelm01@hotmail.co.uk>
2020-01-02 18:33:10 +0000

    add python3-pip as part of initial installation

--- initial_install.sh
+++ initial_install.sh
@@ -1,4 +1,4 @@
 sudo apt install -y steam
-sudo apt install -y entr zsh python python3 autojump silversearcher-ag tmux
+sudo apt install -y entr zsh python python3 autojump silversearcher-ag tmux python3-pip
 # For the pyenv dependencies
 sudo apt install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
