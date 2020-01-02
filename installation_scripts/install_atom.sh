6e3a269beb23f36c5b74804ba1b65d901eb7e940
pavelexpertov <pavelm01@hotmail.co.uk>
2020-01-02 18:02:00 +0000

    add atom installation script

---
+++ installation_scripts/install_atom.sh
@@ -0,0 +1,6 @@
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt update
+sudo apt-get install atom
+
+apm install hydrogen vim-mode-plus ex-mode
