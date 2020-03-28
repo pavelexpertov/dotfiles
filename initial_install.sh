97e2e96557e87c7843cc98210282b801fe259db0
pavelexpertov <pavelm01@hotmail.co.uk>
2020-03-28 21:52:36 +0000

    add vagrant installation

--- initial_install.sh
+++ initial_install.sh
@@ -1,4 +1,11 @@
-sudo apt install -y steam
-sudo apt install -y entr zsh python python3 autojump silversearcher-ag tmux python3-pip
+sudo apt update
+# Shell related packates
+sudo apt install -y zsh tmux 
+# Utility packages
+sudo apt install -y unzip entr autojump silversearcher-ag 
+# Python related packages
+sudo apt install -y python python3 python3-pip
 # For the pyenv dependencies
 sudo apt install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+# Misc. packages
+sudo apt install -y steam
