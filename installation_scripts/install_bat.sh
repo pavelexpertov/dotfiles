6c1b3449cf53d7c3e75fb9f891eea5d97bd4969f
pavelexpertov <pavelm01@hotmail.co.uk>
2022-09-17 20:13:01 +0100

    Forgot to add -y for apt call

--- installation_scripts/install_bat.sh
+++ installation_scripts/install_bat.sh
@@ -2,7 +2,7 @@ if [[ -z "`type -P bat`" ]]
 then
     wget https://github.com/sharkdp/bat/releases/download/v0.18.0/bat-musl_0.18.0_amd64.deb -O /tmp/bat.deb
     cd /tmp
-    sudo apt install ./bat.deb
+    sudo apt install -y ./bat.deb
 else
     echo "bat is installed"
 fi
