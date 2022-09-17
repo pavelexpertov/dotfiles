a6bfe813f7390a53a235363bbba0b203cc5e8b7e
pavelexpertov <pavelm01@hotmail.co.uk>
2022-09-17 23:39:12 +0100

    Add kubectx to brew install

--- brew_install_script.sh
+++ brew_install_script.sh
@@ -1,6 +1,8 @@
 all_packages=""
 # CLI tools
 all_packages+=" bat wget tmux fasd entr the_silver_searcher ripgrep htop jq jid fd gh"
+# Cloud/Containers related stuff
+all_packages+=" kubectx"
 # Jupyter related stuff
 all_packages+=" pandoc"
 # misc. dependencies
