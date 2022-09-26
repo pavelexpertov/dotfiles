06a1c04956956b2f9d6f3586ed6edd831bf06d06
pavelexpertov <pavelm01@hotmail.co.uk>
2022-09-26 19:33:24 +0100

    Add a clean branches extension for gh

--- installation_scripts/install_gh.sh
+++ installation_scripts/install_gh.sh
@@ -5,6 +5,8 @@ then
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
     sudo apt update
     sudo apt install gh -y
+
+    gh extension install davidraviv/gh-clean-branches
 else
     echo "gh is installed"
 fi
